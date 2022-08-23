ca4ea90a5fd694981cfadf6297bfa37b67ffa934
kaushik-pilligundla <kaushikpilligundla@gmai.com>
2022-08-22 18:25:09 -0700

    binary search using recursion

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-s = "hello"
+def search(arr, target, l, r):
+    if l > r:
+        return -1
+    mid = (l + r) // 2
+    if arr[mid] == target:
+        return mid
+    if target > arr[mid]:
+        return search(arr, target, mid + 1, r)
+    return search(arr, target, l, mid - 1)
 
-l1 = []  # list to store lower case char
 
-for c in s:
-    if "A" <= c <= "Z":  # check whether the char is uppercase
-        unicode = ord(c) + 32  # add 32 unicode value to it
-        char = chr(unicode)  # convert into Char
-        l1.append(char)
-    else:
-        l1.append(c)  # add to list as it is in lowercase
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 5
+l = 0
+r = len(arr) - 1
 
-print("".join(l1))
+print(search(arr, target, l, r))
